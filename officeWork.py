a70b48e6a7c7dcd9fdc57285959e90bb15ea3a6a
Diversification <startsevprm@gmail.com>
2023-05-30 17:28:41 +0500

    add array3

--- officeWork.py
+++ officeWork.py
@@ -12,3 +12,7 @@ print(filtered_array1)
 array2 = ["1234", "1567", "-2", "computer science"]
 filtered_array2 = filter_strings(array2)
 print(filtered_array2)
+
+array3 = ["Russia", "Denmark", "Kazan"]
+filtered_array3 = filter_strings(array3)
+print(filtered_array3)
