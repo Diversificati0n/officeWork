e2b12cb2fa361405cd8fbc6217555a9a440863bd
Diversification <startsevprm@gmail.com>
2023-05-30 17:28:19 +0500

    add array2

--- officeWork.py
+++ officeWork.py
@@ -8,3 +8,7 @@ def filter_strings(strings):
 array1 = ["hello", "2", "world", ":-)"]
 filtered_array1 = filter_strings(array1)
 print(filtered_array1)
+
+array2 = ["1234", "1567", "-2", "computer science"]
+filtered_array2 = filter_strings(array2)
+print(filtered_array2)
